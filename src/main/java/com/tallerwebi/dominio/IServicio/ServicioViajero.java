aca04977e76042c1203c93c7e8345d168209fcc7
lucas <lucaasbj98@gmail.com>
2025-09-30 10:21:31 -0300

    agrego servicio implementacion

--- src/main/java/com/tallerwebi/dominio/IServicio/ServicioViajero.java
+++ src/main/java/com/tallerwebi/dominio/IServicio/ServicioViajero.java
@@ -2,14 +2,15 @@ package com.tallerwebi.dominio.IServicio;
 
 import com.tallerwebi.dominio.Entity.Viajero;
 import com.tallerwebi.dominio.excepcion.CredencialesInvalidas;
-import com.tallerwebi.dominio.excepcion.EdadInvalida;
+import com.tallerwebi.dominio.excepcion.DatoObligatorioException;
+import com.tallerwebi.dominio.excepcion.EdadInvalidaException;
 import com.tallerwebi.dominio.excepcion.UsuarioExistente;
 import com.tallerwebi.dominio.excepcion.UsuarioInexistente;
 
 public interface ServicioViajero {
 
     Viajero login(String nombreUsuario, String contrasenia) throws CredencialesInvalidas;
-    Viajero registrar(Viajero nuevoViajero) throws UsuarioExistente, EdadInvalida;
+    Viajero registrar(Viajero nuevoViajero) throws UsuarioExistente, EdadInvalidaException ,DatoObligatorioException;
     Viajero obtenerViajero(Long viajeroId) throws UsuarioInexistente;
 
 }
---
+++ src/main/java/com/tallerwebi/dominio/excepcion/EdadInvalidaException.java
@@ -0,0 +1,8 @@
+package com.tallerwebi.dominio.excepcion;
+
+public class EdadInvalidaException extends Exception {
+    public EdadInvalidaException(String message) {
+        super(message);
+    }
+
+}
