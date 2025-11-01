bef36a34f9270fedf76d10089a72e897deceef7e
JuanMM27 <juanartin9@gmail.com>
2025-11-01 14:16:21 -0300

    Preparacion para cancelacion de vehiculo

--- src/main/java/com/tallerwebi/dominio/Enums/EstadoVerificacion.java
+++ src/main/java/com/tallerwebi/dominio/Enums/EstadoVerificacion.java
@@ -4,5 +4,6 @@ public enum EstadoVerificacion {
 
     PENDIENTE,    // El conductor o el viaje fue creado, pero aún no se validó la documentación
     VERIFICADO,// El estado final: el usuario o vehículo está validado y habilitado para publicar viajes
-    NO_CARGADO
+    NO_CARGADO,
+    DESACTIVADO
 }
