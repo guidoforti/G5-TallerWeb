ca2f75923bc38d64205a58b15148f0967e6e578d
IgnacioPascale00 <nachipascale@gmail.com>
2025-11-17 11:45:21 -0300

    refactor boton cancelar, nombre y apellido en registro, sacar el ID en historial decisiones, agrego boton volver a detalle de viaje en historial reservas

--- src/main/java/com/tallerwebi/presentacion/Controller/ControladorHistorialReserva.java
+++ src/main/java/com/tallerwebi/presentacion/Controller/ControladorHistorialReserva.java
@@ -1,8 +1,10 @@
 package com.tallerwebi.presentacion.Controller;
 
 import com.tallerwebi.dominio.Entity.Usuario;
+import com.tallerwebi.dominio.Entity.Viaje;
 import com.tallerwebi.dominio.IServicio.ServicioHistorialReserva;
 import com.tallerwebi.dominio.IServicio.ServicioNotificacion;
+import com.tallerwebi.dominio.IServicio.ServicioViaje;
 import com.tallerwebi.presentacion.DTO.OutputsDTO.HistorialReservaDTO;
 import com.tallerwebi.dominio.excepcion.UsuarioNoAutorizadoException;
 import com.tallerwebi.dominio.excepcion.ViajeNoEncontradoException;
@@ -26,15 +28,17 @@ public class ControladorHistorialReserva {
     private ServicioHistorialReserva servicioHistorialReserva;
     private final ServicioNotificacion servicioNotificacion;
     private final ServicioConductor servicioConductor;
+    private final ServicioViaje servicioViaje;
     private static final String ATTRIBUTE_ID_USUARIO = "idUsuario";
     private static final String ATTRIBUTE_ROL = "ROL";
     private static final String ROLES_CONDUCTOR = "CONDUCTOR";
 
     public ControladorHistorialReserva(ServicioHistorialReserva servicioHistorialReserva,
-                                       ServicioConductor servicioConductor, ServicioNotificacion servicioNotificacion) {
+                                       ServicioConductor servicioConductor, ServicioNotificacion servicioNotificacion, ServicioViaje servicioViaje) {
         this.servicioHistorialReserva = servicioHistorialReserva;
         this.servicioConductor = servicioConductor;
         this.servicioNotificacion = servicioNotificacion;
+        this.servicioViaje = servicioViaje;
     }
 
     /**
@@ -60,6 +64,10 @@ public class ControladorHistorialReserva {
             Long usuarioId = (Long) usuarioIdObj;
             modelAndView.addObject("contadorNotificaciones", servicioNotificacion.contarNoLeidas(usuarioId));
             Usuario usuarioEnSesion = servicioConductor.obtenerConductor(usuarioId);
+
+            Viaje viaje = servicioViaje.obtenerViajePorId(idViaje);
+            modelAndView.addObject("origenNombre", viaje.getOrigen().getNombre());
+            modelAndView.addObject("destinoNombre", viaje.getDestino().getNombre());
             // 2. Llamada al servicio que contiene la lógica de autorización
             List<HistorialReservaDTO> historial = 
                     servicioHistorialReserva.obtenerHistorialPorViaje(idViaje, usuarioEnSesion);
@@ -67,7 +75,7 @@ public class ControladorHistorialReserva {
             // 3. Éxito: Agregar datos al modelo
             modelAndView.addObject("historialReservas", historial);
             modelAndView.addObject("idViaje", idViaje);
-            modelAndView.addObject("mensajeExito", "Historial cargado correctamente para el viaje ID: " + idViaje);
+            modelAndView.addObject("mensajeExito", "Historial cargado correctamente.");
             
         } catch (UsuarioInexistente e) {
             session.invalidate();
